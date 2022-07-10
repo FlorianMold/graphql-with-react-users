6e4d7f4efe8ba5cf14c5ca50f0894c30cc600040
Florian Mold <florian.mold@live.at>
2022-07-10 15:31:53 +0200

    Registering GraphQL with Express

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+/** The GraphQL Layer of the application. */
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
+
+/** 
+ * Route the requests for /graphql to the graphql handler.
+ */
+app.use('/graphql', graphqlHTTP({
+  /** graphiql should only be used in a development environment */
+  graphiql: true
+}));
 
 /** 
  * Setting up our application to listen to port 4000.
  */
 app.listen(4000, () => {
-
+  console.log('Listening');
 });
